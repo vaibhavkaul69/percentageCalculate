a2e40f064733d4869cf428bc1f48b80bcb036800
unknown <vaibhav.kaul2@gnmail.com>
2020-04-11 00:44:21 +0530

    Icons for PWA Updated

--- service-worker.js
+++ service-worker.js
@@ -3,6 +3,14 @@ const assets = [
   
   "index.html",
   "calc-img.jpg",
+  "calc-img-72x72.png",
+  "calc-img-96x96.png",
+  "calc-img-144x144.png",
+  "calc-img-128x128.png",
+  "calc-img-152x152.png",
+  "calc-img-196x196.png",
+  "calc-img-384x384.png",
+  "calc-img-512x512.png"
   
 ]
 
